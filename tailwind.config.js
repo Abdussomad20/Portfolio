98e50da6867017c702459a3a642fab363566604b
Abdussomad20 <harzkid2018@gmail.com>
2023-01-05 14:47:29 +0100

    my portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    colors:{
+      
+      
+    },
+    screens:{
+      sm: "480px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
